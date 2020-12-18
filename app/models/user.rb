3d204a07dcfd07d2345c8deb908c67c46a4969f0
jfchen19 <jchenf19@gmail.com>
2020-12-18 20:53:55 +0800

    user add name column

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def display_name
+    name || email
+  end
+  
 end
