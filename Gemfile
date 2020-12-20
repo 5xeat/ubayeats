d8362401fecf6993a06a9b5aaeb3f335bcf2da96
eason <acontentedmind@gmail.com>
2020-12-20 17:00:17 +0800

    role_pundit(driver&store)

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'figaro'
 
 gem 'aasm'
 gem 'carrierwave', '~> 2.1'
+gem "pundit"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/policies/role_policy.rb
@@ -0,0 +1,15 @@
+class RolePolicy < ApplicationPolicy
+  def delivery
+    user && user.role == 'driver'
+  end
+
+  def start_business
+    user && user.role == 'store'
+  end
+  
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
