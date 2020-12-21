17e6235b1830e3e95a48ed2beac41b8dbe7fbef6
eason <acontentedmind@gmail.com>
2020-12-21 20:48:31 +0800

    feature/fixRole_judgment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
       link_to('成為外送員', new_drivers_path, class: 'nav-link') + link_to('成為合作店家', new_stores_path, class: 'nav-link') 
    else
      if current_user.role == 'driver'
-       link_to '開始外送', new_drivers_path, class: 'nav-link'
+       link_to '開始外送', '#', class: 'nav-link'
      else
-       link_to '成為合作店家', new_stores_path, class: 'nav-link'
+       link_to '開店', '#', class: 'nav-link'
      end
    end
   end
