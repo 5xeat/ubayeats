60dd0e1037093b0346ba632c710122b252c7e743
jfchen19 <jchenf19@gmail.com>
2020-12-23 18:18:27 +0800

    user add phone and address column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_081232) do
+ActiveRecord::Schema.define(version: 2020_12_23_093920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_12_19_081232) do
     t.string "role", default: "user"
     t.string "provider"
     t.string "uid"
+    t.string "phone"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
