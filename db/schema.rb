5e78fe7bf3e24243394c0272ed9d07e0ccb17ec1
s8852s <s8852s@hotmail.com.tw>
2020-12-25 00:56:16 +0800

    remove_available_from_product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_032919) do
+ActiveRecord::Schema.define(version: 2020_12_24_165023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_12_24_032919) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
-    t.boolean "available", default: false
     t.string "state", default: "unavailable"
     t.index ["deleted_at"], name: "index_products_on_deleted_at"
     t.index ["user_id"], name: "index_products_on_user_id"
