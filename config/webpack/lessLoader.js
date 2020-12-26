04a7d809504f9321263c7bcf1b66d5f1de4b8353
Ting <0917355950teresa@gmail.com>
2020-12-26 23:22:13 +0800

    ant design change theme config

---
+++ config/webpack/lessLoader.js
@@ -0,0 +1,20 @@
+const getStyleRule = require('@rails/webpacker/package/utils/get_style_rule')
+const { getThemeVariables } = require('antd/dist/theme')
+
+module.exports = getStyleRule(/\.less$/i, false, [
+  {
+    loader: 'less-loader',
+    options: {
+      lessOptions: {
+        modifyVars: {
+          ...getThemeVariables({
+            dark: true,
+            compact: true,
+          }),
+          'primary-color': "#39c486", 
+        },
+        javascriptEnabled: true,
+      },
+    },
+  }
+])
