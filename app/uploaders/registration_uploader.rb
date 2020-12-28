6635fa6193fc17f11829e07798299f5b98954288
eason <acontentedmind@gmail.com>
2020-12-28 16:55:25 +0800

    修改圖片上傳地點，gem版本，登入要求

--- app/uploaders/registration_uploader.rb
+++ app/uploaders/registration_uploader.rb
@@ -1,4 +1,4 @@
-class LicenseUploader < CarrierWave::Uploader::Base
+class RegistrationUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -29,15 +29,15 @@ class LicenseUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
