048ea37b2ac033106227522ba43720daa6f9cba5
jfchen19 <jchenf19@gmail.com>
2020-12-29 11:05:44 +0800

    rspec1. 可以將餐點加入購物車，購物車就有東西了

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "可以將餐點加入購物車，購物車就有東西了" do
+    cart = Cart.new
+    cart.add_item 1
+    expect(cart.empty?).to be false
+  end
 end
