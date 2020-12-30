f73220c241af70a35367164ee002a0b497d8ea7b
eason <acontentedmind@gmail.com>
2020-12-30 22:19:29 +0800

    只差清空輸入消息內容和排版

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,30 +1,39 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id:2}, {
-  connected() {
-    console.log("yeah")
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = room_element.getAttribute('data-room-id');
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
 
-  received(data) {
-    console.log(data)
-    const element = document.getElementById('user-id');
-    const user_id = Number(element.getAttribute('data-user-id'));
-
-    let html;
-
-    if (user_id === data.message.user_id){
-      html = data.mine
-    } else {
-      html = data.theirs
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
+    connected() {
+      console.log("已連結上" + room_id)
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      console.log(data)
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+  
+      let html;
+  
+      if (user_id === data.message.user_id){
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+  
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+      // Called when there's incoming data on the websocket for this channel
     }
-
-    const messageContainer = document.getElementById('messages')
-    messageContainer.innerHTML = messageContainer.innerHTML + html
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+  });
+})
