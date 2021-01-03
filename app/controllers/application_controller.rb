411dbee2dc258ef3aad01f9fc26f7613f250ba68
Ting <0917355950teresa@gmail.com>
2021-01-03 13:35:08 +0800

    rescue_from_notFound404/pudit422

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorize
+
   def session_required
     redirect_to sign_in_users_path, notice: '請先登入會員' if not user_signed_in?
   end
@@ -13,5 +16,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:phone])
     devise_parameter_sanitizer.permit(:account_update, keys: [:address])
+  end
+
+  def record_not_found
+    render file: 'public/404.html', layout: false, status: 404
+  end
+
+  def not_authorize
+    render file: 'public/422.html', layout: false, status: 422
   end
 end
