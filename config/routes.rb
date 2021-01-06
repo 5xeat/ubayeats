820df40a9c5b2b01347e0b4cd8718a915504f1d0
tingyiiii <73389929+tingyiiii@users.noreply.github.com>
2021-01-06 13:27:45 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,22 +13,14 @@ Rails.application.routes.draw do
   
   resources :orders
 
-<<<<<<< HEAD
   resource :carts do 
     post 'add_item/:id', action: 'add_item', as: 'add_item'
-=======
-  resource :carts do
->>>>>>> develop
     get :checkout
     collection do
       post :pay
       get :confirm
     end
   end
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
 
   resources :rooms
   resources :messages
