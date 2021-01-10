dc764559bf9af96fa0ea09f9c28403d821236cab
s8852s <s8852s@hotmail.com.tw>
2021-01-11 00:58:57 +0800

    home#distance_filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,22 @@ class HomeController < ApplicationController
   end
 
   def distance_filter
-
+    lat = params[:latitude]
+    lng = params[:longitude]
+    lat1 = 25.042493505554233
+    lng1 = 121.51374312619893
+    p '==============================='
+    p params[:latitude], params[:longitude]
+    # p lat
+    p '==============================='
+    lat = params[:latitude]
+    lng = params[:longitude]
+    if params[:latitude]
+      stores = StoreProfile.calc_distance(lat1, lng1)
+      StoreProfile.where(id: stores)
+      p '==============================='
+      p StoreProfile.where(id: stores)
+      p '==============================='
+    end
   end
 end
