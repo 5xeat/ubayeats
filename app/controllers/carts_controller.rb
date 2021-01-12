db734cb953fca8ca45f62a2c75c6a9b35174cbdc
Ting <0917355950teresa@gmail.com>
2021-01-12 21:53:29 +0800

    fix change linepay confirmUrl

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,7 +58,7 @@ class CartsController < ApplicationController
     trade_no = "UB#{Time.zone.now.to_i}"
     body = {
             "amount": current_cart.total_price,
-            "confirmUrl":"https://ubayeats.herokuapp.com/confirm",
+            "confirmUrl":"https://ubayeats.herokuapp.com/carts/confirm",
             "productName":"Ubayeats",
             "orderId": @order.num,
             "currency": "TWD"
