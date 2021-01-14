2e009efd11d6ba019b2d58ac047134c581cf61b3
s8852s <54518817+s8852s@users.noreply.github.com>
2021-01-14 12:15:11 +0800

    Update connection.rb

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,30 +1,5 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
-
-    # def connect
-    #   self.current_user = find_verified_user
-    # end
-
-    # # def connet_store
-    # #   self.current_user= find_verified_store
-    # # end
-
-    # private
-    #   def find_verified_user
-    #     if verified_user = env['warden.user']
-    #       verified_store = verified_user.orders.last.store_profile
-    #       verified_store
-    #     else
-    #       reject_unauthorized_connection
-    #     end
-    #   end
-    #   def find_verified_store
-    #     if verified_store = User.find_by(id: cookies.encrypted[:user_id]).order
-    #       verified_user
-    #     else
-    #       reject_unauthorized_connection
-    #     end
-    #   end
+    
   end
 end
