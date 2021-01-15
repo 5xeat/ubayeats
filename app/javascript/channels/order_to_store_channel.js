d2daa6cb83ae4c9064f6ca4c01c38ddd6492fd02
s8852s <s8852s@hotmail.com.tw>
2021-01-15 16:38:27 +0800

    修改店家收到通知時未指定js執行條件的bug

--- app/javascript/channels/order_to_store_channel.js
+++ app/javascript/channels/order_to_store_channel.js
@@ -15,8 +15,17 @@ consumer.subscriptions.create("OrderToStoreChannel", {
     if(data.receiver === store){
       let notice = document.createElement('div')
       notice.textContent = `您有一筆${data.price}元的訂單，請立即查看!訂單編號${data.orderId}`
-      document.querySelector('.right').insertAdjacentElement('afterbegin', notice)
-      document.querySelector('.recieving').classList.add("bg-red-600");
+      const $right = document.querySelector('.right')
+      $right && $right.insertAdjacentElement('afterbegin', notice)
+      const $recieving = document.querySelector('.recieving')
+      $recieving && $recieving.classList.add("bg-red-600");
+
+      // form = document.createElement('div')
+      // document.querySelector('.store_profiles.show .edit_order').insertAdjacentElement('afterbegin', )
+      // 新訂單畫面演戲 後補
     } 
+
+
+
   }
 });
