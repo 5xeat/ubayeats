2f2a9b30820ba3120f35a6bdb574f9dd31a8a78a
s8852s <s8852s@hotmail.com.tw>
2021-01-15 16:41:56 +0800

    使用者收到通知的同時進度條同步改變

--- app/javascript/channels/order_to_store_channel.js
+++ app/javascript/channels/order_to_store_channel.js
@@ -24,8 +24,29 @@ consumer.subscriptions.create("OrderToStoreChannel", {
       // document.querySelector('.store_profiles.show .edit_order').insertAdjacentElement('afterbegin', )
       // 新訂單畫面演戲 後補
     } 
-
-
-
+    
+    let user = JSON.parse(document.querySelector('#navbar').getAttribute('data')).id
+    console.log(`${data.receiver} === ${user}`)
+    if(data.receiver === user){
+      if (data.order_state=== 'preparing'){
+        console.log(data.order_state)
+        console.log(document.querySelector(".j-preparing"))
+        document.querySelector(".j-preparing").classList.remove('bg-gray-300')
+        document.querySelector(".j-preparing").classList.add('bg-red-300')
+        document.querySelector(".j-order-state-text").textContent = '店家已接受您的訂單，正在準備中...'
+      } else if (data.order_state === 'delivering') {
+        document.querySelector(".j-delivering").classList.remove('bg-gray-300')
+        document.querySelector(".j-delivering").classList.add('bg-red-300')
+        document.querySelector(".j-order-state-text").textContent = '店家已完成您的餐點，正在等待外送員領取...'
+      } else if (data.order_state=== 'completed') {
+        document.querySelector(".j-completed").classList.remove('bg-gray-300')
+        document.querySelector(".j-completed").classList.add('bg-red-300')
+        document.querySelector(".j-order-state-text").textContent = '外送員已領取餐點，正在前往您的位置...'
+      }else if (data.order_state=== 'arrived') {
+        document.querySelector(".j-arrived").classList.remove('bg-gray-300')
+        document.querySelector(".j-arrived").classList.add('bg-red-300')
+        document.querySelector(".j-order-state-text").textContent = '外送員已抵達，請準備取餐!'
+      }
+    }
   }
 });
