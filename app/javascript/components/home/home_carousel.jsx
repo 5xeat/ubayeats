76107f7fde0dab8af9c1884678f8700008287d81
Ting <0917355950teresa@gmail.com>
2021-01-17 21:44:30 +0800

    fix carousel_img_url

--- app/javascript/components/home/home_carousel.jsx
+++ app/javascript/components/home/home_carousel.jsx
@@ -6,7 +6,7 @@ function HomeCarousel(){
   return(
     <Carousel autoplay={true}>
       <div className="carousel">
-        <img src="https://i.imgur.com/IIlCdI5.png" style={{width: '100%'}} />
+        <img src="https://i.imgur.com/RHnjONq.png" style={{width: '100%'}} />
       </div>
       <div className="carousel">
         <img src="https://i.imgur.com/YPJzLJB.png" style={{width: '100%'}} />
